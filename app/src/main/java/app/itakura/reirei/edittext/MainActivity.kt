c03742980093312cb7ba2cf95d095993a1c74f66
mirei <usrmii301@gmail.com>
2020-09-15 15:00:51 +0900

    editTextスキルリスト

---
+++ app/src/main/java/app/itakura/reirei/edittext/MainActivity.kt
@@ -0,0 +1,20 @@
+package app.itakura.reirei.edittext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            if(editText != null){
+                textView.text = editText.text.toString()
+
+
+            }
+        }
+    }
+}
